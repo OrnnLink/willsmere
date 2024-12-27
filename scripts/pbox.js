dc507be0c269b7d4e22ff53085c91af5c25c6f95
OrnnLink <daniellay.work@hotmail.com>
2024-12-27 07:56:46 +0000

    Update pbox.js

--- scripts/pbox.js
+++ scripts/pbox.js
@@ -32,8 +32,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     pbox = pbox.split("\n");
 
     const [firstnames, lastnames, data] = filtered_data(pbox);
-	console.log(firstnames);
-	console.log(lastnames);
     const fnSearchBar = document.getElementById("firstname-search");
     const fnDatalist = document.getElementById("firstname-list");
 
@@ -242,12 +240,7 @@ const update_history = (fn, ln, list) => {
 			    const [lastname, firstname] = name.split(", ");
 			    fnSearchBar.value = firstname;
 			    lnSearchBar.value = lastname;
-				const result = search(fnSearchBar.value.toLowerCase(), lnSearchBar.value.toLowerCase(), data); 
-    	if (result) {
-    		// display_result(result);
-    		display_search_result(result);
-    		update_history(fnSearchBar.value, lnSearchBar.value, pbox_history);
-    	}
+	
 
 			});
 		}
